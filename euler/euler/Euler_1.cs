d28b87aa7bba09820fbef9ecf28a59e67507f080
HamritSidhu <hamritsidhu_94@hotmail.com>
2015-03-12 23:36:01 -0400

    Refactored project structure for easily evaluating results on console.  Added solution for problem 2.

--- euler/euler/Euler_1.cs
+++ euler/euler/Euler_1.cs
@@ -6,31 +6,19 @@
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
-namespace Euler_1
+namespace euler
 {
     class _solution1
     {
-        static void Main(string[] args)
-        {
-            _solution1 solution = new _solution1();
-            //Printing out result
-            int answer = solution.sumof3and5multiples(1000);
-            Console.WriteLine(answer);
-            Console.ReadLine();
-        }
-
-
         //Solution
-        int sumof3and5multiples(int below)
+        public int sumof3and5multiples(int below)
         {
             int sum = 0;
 
             for (int i = 1; i < below; i++)
             {
                 if (i % 3 == 0 || i % 5 == 0)
-                {
                     sum += i;
-                }
             }
 
             return sum;
---
+++ euler/euler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace euler
+{
+    class Program
+    {
+        //Used to print out answers on console
+        static void Main(string[] args) { 
+            _solution2 solution = new _solution2();
+            int answer = solution.fibEvenSum(4000000);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
