f726701f76d23c74f307f463ec886b4fc0bd5460
HamritSidhu <hamritsidhu_94@hotmail.com>
2015-03-19 20:40:03 -0400

    solution to problem 4

---
+++ euler/euler/Euler_4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+/*
+ * A palindromic number reads the same both ways. 
+ * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+namespace euler
+{
+    class _solution4
+    {
+        public int largestPalindrome(int numDigits)
+        {
+            int smallest = (int)Math.Pow(10, numDigits - 1);
+            int largest = (int)Math.Pow(10, numDigits) - 1;
+            int num = 0;
+            
+            for (int i = smallest; i <= largest; i++) {
+                for (int j = i; j <= largest; j++) {
+                    if (isPalindrome(i * j) && (i*j)>num)
+                        num = i * j;
+                }
+            }
+            return num;
+        }
+
+        public bool isPalindrome(int num) {
+            string stringNum = num.ToString();
+            char[] chars = stringNum.ToCharArray();
+            Array.Reverse(chars);
+            string reversed = new string(chars);
+            return stringNum == reversed;
+        }
+
+        public void solve() {
+            int result = this.largestPalindrome(3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        //ALTERNATE WAY OF CHECKING FOR PALINDROME
+        //public bool isPalindrome(int num) {
+        //    string number = num.ToString();
+        //    ASCIIEncoding encode = new ASCIIEncoding();
+        //    byte[] charAsciis = encode.GetBytes(number);
+
+        //    int i = 0;
+        //    int j = charAsciis.Length - 1;
+        //    while (i <= j)
+        //    {
+        //        if (charAsciis[i] != charAsciis[j])
+        //            return false;
+        //        i++;
+        //        j--;
+        //    }
+        //    return true;
+        //}
+    }
+}
--- euler/euler/Program.cs
+++ euler/euler/Program.cs
@@ -7,7 +7,7 @@ namespace euler
     {
         //Used to print out answers to a solution on console
         static void Main(string[] args) { 
-            _solution3 solution = new _solution3();
+            _solution4 solution = new _solution4();
             solution.solve();
         }
     }
